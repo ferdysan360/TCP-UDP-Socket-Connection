51f5d042eafc2db1c4781791017d7ccc25da69bf
ChristzenLeonardy <christzenleonardy@gmail.com>
2019-09-26 05:02:24 +0700

    add useful constants

---
+++ constant.py
@@ -0,0 +1,7 @@
+DATA = str(0x00).encode()
+ACK = str(0x01).encode()
+FIN = str(0x02).encode()
+FIN_ACK = str(0x03).encode()
+MAX_DATA_LENGTH = 32768
+MAX_PACKET_LENGTH = 65536
+ONES = "1111111111111111".encode()
